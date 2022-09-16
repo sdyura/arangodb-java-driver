bfef4a59a159477ee232610823ec020fe4474158
Michele Rastelli <michele@arangodb.com>
2022-09-16 21:04:44 +0200

    removed FastText analyzers tests

--- src/test/java/com/arangodb/ArangoSearchTest.java
+++ src/test/java/com/arangodb/ArangoSearchTest.java
@@ -1147,52 +1147,52 @@ class ArangoSearchTest extends BaseJunit5 {
         createGetAndDeleteTypedAnalyzer(db, collationAnalyzer);
     }
 
-    @ParameterizedTest(name = "{index}")
-    @MethodSource("dbs")
-    void classificationAnalyzer(ArangoDatabase db) {
-        assumeTrue(isAtLeastVersion(3, 10));
-        assumeTrue(isEnterprise());
-
-        ClassificationAnalyzerProperties properties = new ClassificationAnalyzerProperties();
-        properties.setModelLocation("/tmp/foo.bin");
-        properties.setTopK(2);
-        properties.setThreshold(.5);
-
-        Set<AnalyzerFeature> features = new HashSet<>();
-        features.add(AnalyzerFeature.frequency);
-        features.add(AnalyzerFeature.norm);
-        features.add(AnalyzerFeature.position);
-
-        ClassificationAnalyzer analyzer = new ClassificationAnalyzer();
-        analyzer.setName("test-" + UUID.randomUUID());
-        analyzer.setProperties(properties);
-        analyzer.setFeatures(features);
-
-        createGetAndDeleteTypedAnalyzer(db, analyzer);
-    }
-
-    @ParameterizedTest(name = "{index}")
-    @MethodSource("dbs")
-    void nearestNeighborsAnalyzer(ArangoDatabase db) {
-        assumeTrue(isAtLeastVersion(3, 10));
-        assumeTrue(isEnterprise());
-
-        NearestNeighborsAnalyzerProperties properties = new NearestNeighborsAnalyzerProperties();
-        properties.setModelLocation("/tmp/foo.bin");
-        properties.setTopK(2);
-
-        Set<AnalyzerFeature> features = new HashSet<>();
-        features.add(AnalyzerFeature.frequency);
-        features.add(AnalyzerFeature.norm);
-        features.add(AnalyzerFeature.position);
-
-        NearestNeighborsAnalyzer analyzer = new NearestNeighborsAnalyzer();
-        analyzer.setName("test-" + UUID.randomUUID());
-        analyzer.setProperties(properties);
-        analyzer.setFeatures(features);
-
-        createGetAndDeleteTypedAnalyzer(db, analyzer);
-    }
+//    @ParameterizedTest(name = "{index}")
+//    @MethodSource("dbs")
+//    void classificationAnalyzer(ArangoDatabase db) {
+//        assumeTrue(isAtLeastVersion(3, 10));
+//        assumeTrue(isEnterprise());
+//
+//        ClassificationAnalyzerProperties properties = new ClassificationAnalyzerProperties();
+//        properties.setModelLocation("/tmp/foo.bin");
+//        properties.setTopK(2);
+//        properties.setThreshold(.5);
+//
+//        Set<AnalyzerFeature> features = new HashSet<>();
+//        features.add(AnalyzerFeature.frequency);
+//        features.add(AnalyzerFeature.norm);
+//        features.add(AnalyzerFeature.position);
+//
+//        ClassificationAnalyzer analyzer = new ClassificationAnalyzer();
+//        analyzer.setName("test-" + UUID.randomUUID());
+//        analyzer.setProperties(properties);
+//        analyzer.setFeatures(features);
+//
+//        createGetAndDeleteTypedAnalyzer(db, analyzer);
+//    }
+//
+//    @ParameterizedTest(name = "{index}")
+//    @MethodSource("dbs")
+//    void nearestNeighborsAnalyzer(ArangoDatabase db) {
+//        assumeTrue(isAtLeastVersion(3, 10));
+//        assumeTrue(isEnterprise());
+//
+//        NearestNeighborsAnalyzerProperties properties = new NearestNeighborsAnalyzerProperties();
+//        properties.setModelLocation("/tmp/foo.bin");
+//        properties.setTopK(2);
+//
+//        Set<AnalyzerFeature> features = new HashSet<>();
+//        features.add(AnalyzerFeature.frequency);
+//        features.add(AnalyzerFeature.norm);
+//        features.add(AnalyzerFeature.position);
+//
+//        NearestNeighborsAnalyzer analyzer = new NearestNeighborsAnalyzer();
+//        analyzer.setName("test-" + UUID.randomUUID());
+//        analyzer.setProperties(properties);
+//        analyzer.setFeatures(features);
+//
+//        createGetAndDeleteTypedAnalyzer(db, analyzer);
+//    }
 
     @ParameterizedTest(name = "{index}")
     @MethodSource("dbs")
