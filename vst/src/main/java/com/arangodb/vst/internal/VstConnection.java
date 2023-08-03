4145f38d9883264f958f98e85acc003bbb75ce47
Yura <yura@yura.net>
2023-08-03 15:21:08 +0100

    thread name for the Executor
    libs should always set thread names, otherwise people debugging large apps are faced with loads of threads and have no idea where they came from or what they are doing

--- vst/src/main/java/com/arangodb/vst/internal/VstConnection.java
+++ vst/src/main/java/com/arangodb/vst/internal/VstConnection.java
@@ -171,7 +171,15 @@ public abstract class VstConnection<T> implements Connection {
         }
         sendProtocolHeader();
 
-        executor = Executors.newSingleThreadExecutor();
+        Executors.newSingleThreadExecutor(new ThreadFactory() {
+            @Override
+            public Thread newThread(@NotNull Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setName("Arangodb-VstConnection");
+                return thread;
+            }
+        });
+
         executor.submit((Callable<Void>) () -> {
             LOGGER.debug("[" + connectionName + "]: Start Callable");
 
